91217d072026e80825c713088d811e3151408389
reydiazz <angheloreynoso11@gmail.com>
2026-05-12 10:07:24 -0500

    refactor: completar definición de HotelErrorCode con HttpStatus por error

--- src/main/java/com/hotel/booking/api/domain/hotel/exception/HotelErrorCode.java
+++ src/main/java/com/hotel/booking/api/domain/hotel/exception/HotelErrorCode.java
@@ -1,16 +1,27 @@
 package com.hotel.booking.api.domain.hotel.exception;
 
 import com.hotel.booking.api.shared.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 public enum HotelErrorCode implements ErrorCode {
 
-    CODE_ALREADY_EXISTS,
-    NOT_FOUND,
-    HAS_RELATIONS;
+    CODE_ALREADY_EXISTS(HttpStatus.CONFLICT),
+    NOT_FOUND(HttpStatus.NOT_FOUND),
+    HAS_RELATIONS(HttpStatus.CONFLICT);
+
+    private final HttpStatus httpStatus;
+
+    HotelErrorCode(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
 
     @Override
     public String getCode() {
         return name();
     }
 
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
 }
