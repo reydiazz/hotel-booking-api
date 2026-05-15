378a185510321e0b82d9c23585e06244f163c48e
Fabrizio7S <leon.mallma14@gmail.com>
2026-05-15 10:58:54 -0500

    feat: crear entidad person

---
+++ src/main/java/com/hotel/booking/api/domain/person/model/entity/Person.java
@@ -0,0 +1,52 @@
+package com.hotel.booking.api.domain.person.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Persons")
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Person {
+
+    @Id
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    public Person(String code, String firstName, String lastName, String phone, LocalDate birthDate) {
+        this.code = code;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+
+    public void update(String firstName, String lastName, String phone, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+
+
+}
