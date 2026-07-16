dd250aeaee12a10b0347d21b3a915a8d000b9cda
reydiazz <angheloreynoso11@gmail.com>
2026-07-16 13:22:33 -0500

    refactor: usar el repositorio directamente en CustomUserDetailsService

--- src/main/java/com/hotel/booking/api/security/CustomUserDetailsService.java
+++ src/main/java/com/hotel/booking/api/security/CustomUserDetailsService.java
@@ -1,23 +1,25 @@
 package com.hotel.booking.api.security;
 
 import com.hotel.booking.api.domain.auth.model.entity.User;
-import com.hotel.booking.api.domain.auth.service.UserService;
+import com.hotel.booking.api.domain.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor_ = @__({@Lazy}))
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserService service;
+    private final UserRepository repository;
+    private final String message = "USER_NOT_FOUND";
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        User user = service.findByUsername(username);
+        User user = repository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(message)
+        );
         return new UserPrincipal(user);
     }
-
 }
